b9375861b03e1c66390ec4764f9f88f10099651a
mokarakaya <mokarakaya@gmail.com>
2023-03-11 00:52:57 +0100

    update classification

--- src/movielens/collaborative_filtering/collab_captum.py
+++ src/movielens/collaborative_filtering/collab_captum.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+from captum.attr import IntegratedGradients, LayerIntegratedGradients
 
 from src.data_util import get_movielens
 from src.movielens.collaborative_filtering.collab_constants import MODEL_PATH
@@ -6,10 +8,7 @@ from src.movielens.collaborative_filtering.collab_model import CollabFNet
 from src.util import get_device
 
 
-def get_model():
-    device = get_device()
-
-    df_train, df_val = get_movielens()
+def get_model(df_train, device):
     num_users = df_train["user_id"].max() + 1
     num_items = df_train["item_id"].max() + 1
     print(num_users, num_items)
@@ -20,8 +19,25 @@ def get_model():
     model.eval()
     return model
 
+
 def main():
-    model = get_model()
+    device = "cpu"
+    df_train, df_val = get_movielens()
+    model = get_model(df_train, device)
+
+    users = torch.IntTensor(df_val["user_id"][:2].values).to(device)
+    items = torch.IntTensor(df_val["item_id"][:2].values).to(device)
+    # ratings = torch.FloatTensor(df_val["rating"].values).to(device)
+    ratings = df_val["rating"].astype(int).tolist()
+    lig = LayerIntegratedGradients(model, [model.user_emb, model.item_emb])
+    # attr, delta = lig.attribute((users, items), target=ratings, return_convergence_delta=True)
+    attr, delta = lig.attribute((users, items), return_convergence_delta=True)
+    attributions = attr[0]
+    attributions = attributions.sum().squeeze(0)
+    attributions = attributions / torch.norm(attributions)
+    attributions = attributions.cpu().detach().numpy()
+    # importances = np.mean(attr, axis=0)
+    print(attributions)
 
 
 if __name__ == "__main__":
